e2d46a05f88386e4e27da8ae5e6007f67270c2b5
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-06-04 21:20:00 +0530

    Added file of UC2_ENSURE_@_BRIDGELABZ

--- EmailIdValidation.js
+++ EmailIdValidation.js
@@ -1,7 +1,7 @@
 console.log("UC-1");
 //UC-1 To Validate First mandatory part of Email
 function validateEmailwithpattern1(email) {
-   var pattern = RegExp('^[A-Z a-z 0-9]{3,}$');
+   var pattern = RegExp('^[A-Z a-z 0-9 .]{3,}$');
    if(pattern.test(email)){
       console.log("Valid EmailId!");
       return true;
@@ -12,5 +12,22 @@ function validateEmailwithpattern1(email) {
    }
 }
 
-let emailId1 = 'abc';
+let emailId1 = 'abc.xyz';
 validateEmailwithpattern1(emailId1);
+
+console.log("UC-2");
+//UC-2 To Validate Second mandatory part of Email
+function validateEmailwithpattern2(email) {
+   var pattern = RegExp('^[A-Z a-z 0-9 .]+@[0-9 a-z A-Z]+$');
+   if(pattern.test(email)){
+      console.log("Valid EmailId!");
+      return true;
+   }
+   else {
+      console.log("Invalid EmailId!");
+      return false;
+   }
+}
+
+let emailId2 = 'abc.xyz@bridgelabz';
+validateEmailwithpattern2(emailId2);
