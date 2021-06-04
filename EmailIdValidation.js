af928345ef8323b7ba070bd81f022842ac2047b2
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-06-04 21:24:18 +0530

    Added file of UC3_VALIDATE_CO

--- EmailIdValidation.js
+++ EmailIdValidation.js
@@ -31,3 +31,20 @@ function validateEmailwithpattern2(email) {
 
 let emailId2 = 'abc.xyz@bridgelabz';
 validateEmailwithpattern2(emailId2);
+
+console.log("UC-3");
+//UC-3 To Validate Third mandatory part of Email
+function validateEmailwithpattern3(email) {
+   var pattern = RegExp('^[A-Z a-z 0-9 .]+@[0-9 a-z A-Z]+.[a-z A-Z]{2,3}$');
+   if(pattern.test(email)){
+      console.log("Valid EmailId!");
+      return true;
+   }
+   else {
+      console.log("Invalid EmailId!");
+      return false;
+   }
+}
+
+let emailId3 = 'abc.xyz@bridgelabz.co';
+validateEmailwithpattern3(emailId3);
